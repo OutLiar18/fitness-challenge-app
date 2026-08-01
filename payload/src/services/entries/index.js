26368ee952d64e182aa639a88bb8cbd296ce825e
Kyle Pillay <kylep1828@gmail.com>
2026-08-01 11:40:00 +0200

    feat: add Legacy Avatars and announcement status

--- payload/src/services/entries/index.js
+++
@@ -1,9 +0,0 @@
-export {
-  createEntry,
-  createExerciseSuggestion,
-  createLibrarySuggestion,
-  deleteEntry,
-  subscribeToEntries,
-} from "./entryRepository";
-export { normalizeEntry } from "./normalizer";
-export { saveChallengeEntry } from "./entryManager";
